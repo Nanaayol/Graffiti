3c7d7084fdddcbd6d29520100594be95194a8a87
Nana Ayoluwa <akuabamidele@Nanas-MacBook-Pro.local>
2022-08-30 19:13:22 -0400

    modified border radius of all images iterating over array using for loop

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ let DoggosHeader = document.querySelector ('#page-header')
 DoggosHeader.style.textAlign = "left"
 
 let dogImages = querySelectorAll (".dog-image")
+for (let i=0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+
 // Select some elements...
